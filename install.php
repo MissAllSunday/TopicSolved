0276f312e9f1f54c21321402efadadbaddbcf683
Suki <suki@missallsunday.com>
2013-01-29 17:19:20 -0600

    install
    Signed-off-by: Suki <suki@missallsunday.com>

--- install.php
+++ install.php
@@ -1,64 +1,76 @@
 <?php
 
+/**
+ * @package Topic Solved mod
+ * @version 1.0
+ * @author Suki <missallsunday@simplemachines.org>
+ * @copyright 2012 Suki
+ * @license http://www.mozilla.org/MPL/ MPL 2.0
+ */
+
+/*
+ * Version: MPL 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ */
+
 if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
-{
-	$ssi = true;
 	require_once(dirname(__FILE__) . '/SSI.php');
-}
-elseif (!defined('SMF'))
-	exit('<strong>Error:</strong> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
 
-global $smcFunc;
+else if(!defined('SMF'))
+	die('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php and SSI.php files.');
 
-if (!array_key_exists('db_add_column', $smcFunc))
-	db_extend('packages');
+if ((SMF == 'SSI') && !$user_info['is_admin'])
+	die('Admin priveleges required.');
 
-$columns = $smcFunc['db_list_columns']('{db_prefix}topics');
-
-if (!in_array('is_solved', $columns))
-	$smcFunc['db_add_column']('{db_prefix}topics', array('name' => 'is_solved', 'type' => 'tinyint', 'size' => 3));
-
-$columns = $smcFunc['db_list_columns']('{db_prefix}boards');
-
-if (!in_array('topic_solved', $columns))
-	$smcFunc['db_add_column']('{db_prefix}boards', array('name' => 'topic_solved', 'type' => 'tinyint', 'size' => 4));
-
-$request = $smcFunc['db_query']('', '
-	SELECT id_icon
-	FROM {db_prefix}message_icons
-	WHERE filename = {string:name}
-		AND title = {string:title}
-	LIMIT 1',
+/* Create the scheduled task */
+$smcFunc['db_insert'](
+	'insert',
+	'{db_prefix}scheduled_tasks',
 	array(
-		'name' => 'topicsolved',
-		'title' => 'Topic Solved',
+		'id_task' => 'int',
+		'next_time' => 'int',
+		'time_offset' => 'int',
+		'time_regularity' => 'int',
+		'time_unit' => 'string',
+		'disabled' => 'int',
+		'task' => 'string',
+	),
+	array(
+		0, 0, 0, 1, 'd', 0, 'topic_solved',
+	),
+	array(
+		'id_task',
 	)
 );
-list ($exists) = $smcFunc['db_fetch_row']($request);
-$smcFunc['db_free_result']($request);
-
-if (empty($exists))
-{
-	$request = $smcFunc['db_query']('', '
-		SELECT icon_order
-		FROM {db_prefix}message_icons
-		ORDER BY icon_order DESC
-		LIMIT 1',
-		array(
-		)
-	);
-	list ($order) = $smcFunc['db_fetch_row']($request);
-	$smcFunc['db_free_result']($request);
-
-	$smcFunc['db_insert']('',
-		'{db_prefix}message_icons',
-		array('title' => 'string', 'filename' => 'string', 'icon_order' => 'int'),
-		array('Topic Solved', 'topicsolved', ++$order),
-		array('id_icon')
-	);
 }
 
-if (!empty($ssi))
-	echo 'Database installation complete!';
+db_extend('packages');
 
-?>
+// Add the column for figuring out if we've done this user before
+//$smcFunc['db_add_column'] (table_name, column_into, parameters, if_exists, error)
+$smcFunc['db_add_column'](
+	'{db_prefix}topics',
+	array(
+		'name' => 'is_solved',
+		'type' => 'int',
+		'size' => 2,
+		'null' => false,
+		'default' => 0,
+		'unsigned' => true,
+	),
+	array(),
+	'update',
+	null
+);
+
+if (SMF == 'SSI')
+	echo 'Database changes are complete!';
