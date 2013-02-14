6e1048715945526aac66635dbc2aa30e6509265c
Suki <suki@missallsunday.com>
2013-02-13 18:28:46 -0600

    More regular stuff
    Signed-off-by: Suki <suki@missallsunday.com>

--- hooks.php
+++ hooks.php
@@ -31,54 +31,10 @@ else if(!defined('SMF'))
 if ((SMF == 'SSI') && !$user_info['is_admin'])
 	die('Admin priveleges required.');
 
-/* This mod needs PHP 5.3 or greater, sorry... */
-topicsolvedCheck();
+	$hooks = array(
+		'integrate_pre_include' => '$sourcedir/ShareThis.php',
+		'integrate_admin_areas' => 'ShareThis::Admin',
+	);
 
-/* Create the scheduled task */
-$smcFunc['db_insert'](
-	'insert',
-	'{db_prefix}scheduled_tasks',
-	array(
-		'id_task' => 'int',
-		'next_time' => 'int',
-		'time_offset' => 'int',
-		'time_regularity' => 'int',
-		'time_unit' => 'string',
-		'disabled' => 'int',
-		'task' => 'string',
-	),
-	array(
-		0, 0, 0, 1, 'd', 0, 'topic_solved',
-	),
-	array(
-		'id_task',
-	)
-);
-}
-
-db_extend('packages');
-
-/* Add the column */
-$smcFunc['db_add_column'](
-	'{db_prefix}topics',
-	array(
-		'name' => 'is_solved',
-		'type' => 'int',
-		'size' => 2,
-		'null' => false,
-		'default' => 0,
-		'unsigned' => true,
-	),
-	array(),
-	'update',
-	null
-);
-
-if (SMF == 'SSI')
-	echo 'Database changes are complete!';
-
-	function topicsolvedCheck()
-	{
-		if (version_compare(PHP_VERSION, '5.2.0', '<'))
-			fatal_error('This mod needs PHP 5.2 or greater. You will not be able to install/use this mod, contact your host and ask for a php upgrade.');
-	}
+	foreach ($hooks as $hook => $function)
+		add_integration_function($hook, $function);
